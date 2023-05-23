3688f386695db9a67449daedeeff56775c189e7a
Timur <timur.yeskov@gmail.com>
2023-05-23 13:19:17 +0400

    Update Haqqimizda.jsx

--- src/pages/Haqqimizda.jsx
+++ src/pages/Haqqimizda.jsx
@@ -7,7 +7,7 @@ const Haqqimizda=()=>{
         
         <div className="first">
         <Header/>
-            <h1>Haqqimizda</h1>
+            <h1>Haqqımızda</h1>
         </div>
         <div className="second">
             <h1>“KOS Dostu” PASHA Bankın kiçik və orta biznes sahibkarlarına öz idxal və ixrac əməliyyatlarını təhlükəsiz yolla həyata keçirmələrini təmin edən SME məhsuludur.</h1>
